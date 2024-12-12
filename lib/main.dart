bb6155ce1769ad558fdbd672d74f99ed81c1c253
Abdul Rahim <93329242+developer-rahim@users.noreply.github.com>
2024-12-12 20:46:46 +0600

    chore : Added firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:portfolio_website/firebase_options.dart';
 import 'package:portfolio_website/src/core/main_dashboard_binding.dart';
 import 'package:portfolio_website/src/features/dashboard/view/main_dashboard.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
