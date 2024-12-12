53ea724a57e19ef650ef7dad4a1073fda62dcac3
Abdul Rahim <93329242+developer-rahim@users.noreply.github.com>
2024-12-12 23:07:33 +0600

    feat : Added download resume

--- lib/src/features/home/widgets/home_personal_info.dart
+++ lib/src/features/home/widgets/home_personal_info.dart
@@ -20,7 +20,7 @@ class _HomePersonalInfo extends GetView<HomeViewController> {
         FadeInRight(
           duration: const Duration(milliseconds: 1400),
           child: Text(
-            'Mukhtar Ali Khan',
+            'Abdul Rahim',
             style: AppTextStyles.headingStyles(),
           ),
         ),
@@ -42,9 +42,31 @@ class _HomePersonalInfo extends GetView<HomeViewController> {
         FadeInUp(
           duration: const Duration(milliseconds: 1800),
           child: AppButtons.buildMaterialButton(
-              onTap: () {}, buttonName: 'Download CV'),
+              onTap: () {
+                downloadRemotePdf(
+                    'https://drive.google.com/uc?export=download&id=1OAdtQDMQwG_onW2dm0kNbSn8ZFdfytcD',
+                    'rahim.pdf');
+              },
+              buttonName: 'Download CV'),
         ),
       ],
     );
+  }
+
+  void downloadRemotePdf(String url, String fileName) {
+    // Create an anchor element
+    final anchor = html.AnchorElement()
+      ..href = url // The URL of the remote file
+      ..download = fileName // Set the desired download file name
+      ..style.display = 'none'; // Hide the anchor element
+
+    // Append the anchor to the document body
+    html.document.body?.append(anchor);
+
+    // Programmatically trigger a click on the anchor to start the download
+    anchor.click();
+
+    // Remove the anchor element to clean up the DOM
+    anchor.remove();
   }
 }
