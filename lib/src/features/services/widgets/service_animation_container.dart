66559d5129d8b045e02ce0d1113055f8edba79ed
Abdul Rahim <113752743+nextive-rahim@users.noreply.github.com>
2024-12-22 01:49:54 +0600

    chore : fixed hover animation for android and ios

--- lib/src/features/services/widgets/service_animation_container.dart
+++ lib/src/features/services/widgets/service_animation_container.dart
@@ -18,55 +18,54 @@ class _ServiceAnimationContainer extends GetView<ServiceViewController> {
   Widget build(BuildContext context) {
     return Obx(() {
       return AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          width: hover.value ? hoverWidth : width,
-          height: hover.value ? 390 : 380,
-          alignment: Alignment.center,
-          transform:
-              hover.value ? controller.onHoverActive : controller.onHoverRemove,
-          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
-          decoration: BoxDecoration(
-            color: AppColors.bgColor2,
-            borderRadius: BorderRadius.circular(30),
-            border: hover.value
-                ? Border.all(color: AppColors.themeColor, width: 3)
-                : null,
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.black54,
-                spreadRadius: 4.0,
-                blurRadius: 4.5,
-                offset: Offset(3.0, 4.5),
-              )
-            ],
-          ),
-          child: Column(
-            children: [
-              Image.asset(
-                asset,
-                width: 50,
-                height: 50,
-                color: AppColors.themeColor,
-              ),
-              Constants.sizedBox(height: 30.0),
-              Text(
-                title,
-                style: AppTextStyles.montserratStyle(
-                    color: Colors.white, fontSize: 22.0),
-              ),
-              Constants.sizedBox(height: 12.0),
-              Text(
-                'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
-                ' The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,',
-                style: AppTextStyles.normalStyle(fontSize: 14.0),
-                textAlign: TextAlign.center,
-              ),
-              Constants.sizedBox(height: 20.0),
-              AppButtons.buildMaterialButton(
-                  buttonName: 'Read More', onTap: () {})
-            ],
-          ),
-        );
+        duration: const Duration(milliseconds: 300),
+        width: hover.value ? hoverWidth : width,
+        height: hover.value ? 390 : 380,
+        alignment: Alignment.center,
+        transform: hover.value ? controller.onHoverActive : null,
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 24),
+        decoration: BoxDecoration(
+          color: AppColors.bgColor2,
+          borderRadius: BorderRadius.circular(30),
+          border: hover.value
+              ? Border.all(color: AppColors.themeColor, width: 3)
+              : null,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black54,
+              spreadRadius: 4.0,
+              blurRadius: 4.5,
+              offset: Offset(3.0, 4.5),
+            )
+          ],
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              asset,
+              width: 50,
+              height: 50,
+              color: AppColors.themeColor,
+            ),
+            Constants.sizedBox(height: 30.0),
+            Text(
+              title,
+              style: AppTextStyles.montserratStyle(
+                  color: Colors.white, fontSize: 22.0),
+            ),
+            Constants.sizedBox(height: 12.0),
+            Text(
+              'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+              ' The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,',
+              style: AppTextStyles.normalStyle(fontSize: 14.0),
+              textAlign: TextAlign.center,
+            ),
+            Constants.sizedBox(height: 20.0),
+            AppButtons.buildMaterialButton(
+                buttonName: 'Read More', onTap: () {})
+          ],
+        ),
+      );
     });
   }
 }
